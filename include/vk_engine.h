c9620b360443e9f7242d2c75b302b85bd6cc508b
maisvendoo <maisvendoo@yandex.ru>
2024-06-24 21:12:29 +0300

    Заготовка под инициализацию Vulkan

--- include/vk_engine.h
+++ include/vk_engine.h
@@ -3,6 +3,8 @@
 
 #include    <vk_types.h>
 
+#include    <VkBootstrap.h>
+
 #define     SDL_MAIN_HANDLED
 
 #include    <SDL.h>
@@ -16,22 +18,48 @@ class VulkanEngine
 public:
 
     bool isInitialized { false };
+
     int frameNumber {0};
+
     bool stop_rendering {false};
+
     VkExtent2D windowExtent{1360, 768};
 
     struct SDL_Window *window { nullptr };
 
+    VkInstance instance;
+
+    VkDebugUtilsMessengerEXT debug_messeger;
+
+    VkPhysicalDevice choseGPU;
+
+    VkDevice device;
+
+    VkSurfaceKHR surface;
+
     static VulkanEngine &Get();
 
-    ///
+    // Инициализация движка
     void init();
 
+    // Корректная финализация движка
     void cleanup();
 
+    // Цикл отрисовки
     void draw();
 
+    // Главный цикл приложения
     void run();
+
+private:
+
+    void init_vulkan();
+
+    void init_swapchain();
+
+    void init_commands();
+
+    void init_sync_structures();
 };
 
 #endif
